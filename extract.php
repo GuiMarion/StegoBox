c41abd556dab27f821fbde4cb07ebfe3f7b4cf7f
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-06-15 10:17:31 +0200

    menubar on all pages

--- extract.php
+++ extract.php
@@ -4,10 +4,28 @@
 <title>StegoBox</title>
 <meta charset="iso-8859-1">
 <link rel="stylesheet" href="styles/layout.css" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+
 </head>
 <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">StagoBox</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="upload.php">Upload</a></li>
+      <li><a href="append.php">Append</a></li>
+      <li class = "active"><a href="extract.php">Extract</a></li>
+      <li><a href="view.php">View</a></li>
 
+
+    </ul>
+  </div>
+</nav>
 <!-- content -->
 <div class="wrapper row2">
         
--- upload.php
+++ upload.php
@@ -5,9 +5,26 @@
 <meta charset="iso-8859-1">
 <link rel="stylesheet" href="styles/layout.css" type="text/css">
 <!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">StagoBox</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class = "active"><a href="upload.php">Upload</a></li>
+      <li><a href="append.php">Append</a></li>
+      <li><a href="extract.php">Extract</a></li>
+      <li><a href="view.php">View</a></li>
 
+
+    </ul>
+  </div>
+</nav>
 <!-- content -->
 <div class="wrapper row2">
         
