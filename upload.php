91c533146ddcf4458b92b1db93492582d7493544
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-06-15 10:01:43 +0200

    upload jpg should be fine

--- upload.php
+++ upload.php
@@ -21,18 +21,30 @@ if(isset($_FILES['image']))
 	$file_name = $_FILES['image']['name'];
 	$extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')  ,1)  );
 
-	$nom = SITE_ROOT."/img/{$file_name}.{$extension_upload}";
+	if ($extension_upload != ".jpg" && $extension_upload != ".jpeg"){
 
-	$resultat = move_uploaded_file($_FILES['image']['tmp_name'], $nom);
-	if ($resultat) {
-	    echo "Le fichier a été téléchargé
-		   avec succès.\n";
-	} else {
-	    echo "echec\n";
+		echo("Vous ne pouvez télécharger que des images au format jpeg.")
+
+	}
+
+	else{
+
+		$nom = SITE_ROOT."/img/{$file_name}.{$extension_upload}";
+
+		$resultat = move_uploaded_file($_FILES['image']['tmp_name'], $nom);
+		if ($resultat) {
+		    echo "Le fichier a été téléchargé
+			   avec succès.\n";
+		} else {
+		    echo "echec\n";
+		}
+
 	}
 }
 
 ?>
+
+Seulement les images au format jpeg sont supportées. 
 <form method="post" action="upload.php" enctype="multipart/form-data">
 	<input type="file" name="image" />
 
