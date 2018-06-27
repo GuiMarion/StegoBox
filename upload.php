8d3081e933c7108f7b2c207debd64976b6b3e1bd
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-06-27 10:32:00 +0200

    test

--- upload.php
+++ upload.php
@@ -38,7 +38,7 @@ if(isset($_FILES['image']))
 	$file_name = $_FILES['image']['name'];
 	$extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')  ,1)  );
 
-	if ($extension_upload != ".jpg" && $extension_upload != ".jpeg"){
+	/*if ($extension_upload != ".jpg" && $extension_upload != ".jpeg"){
 
 		echo("Vous ne pouvez télécharger que des images au format jpeg.")
 
@@ -57,6 +57,7 @@ if(isset($_FILES['image']))
 		}
 
 	}
+	*/
 }
 
 ?>
