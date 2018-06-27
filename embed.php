18c955faabf76aba390abda4b07f521f1251ff7d
test <test@test.com>
2018-06-27 11:00:42 +0200

    upload

---
+++ embed.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<title>StegoBox</title>
+<meta charset="iso-8859-1">
+<link rel="stylesheet" href="styles/layout.css" type="text/css">
+<!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+</head>
+<body>
+
+<!-- content -->
+<div class="wrapper row2">
+        
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<center>
+<?php
+echo "Prout";
+if(isset($_FILES['image']))
+{ 
+$uploaddir = '/var/www/html/img';
+$uploadfile = $uploaddir . basename($_FILES['image']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+    echo "Le fichier est valide, et a été téléchargé
+           avec succès.\n";
+} else {
+    echo "echec\n";
+}
+}
+
+?>
+<form method="post" action="index.php" enctype="multipart/form-data">
+	<input type="file" name="image" />
+
+	<!-- MAX_FILE_SIZE doit précéder le champ input de type file -->
+  <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+  
+<input type="submit" name="submit" value="Envoyer" />
+</form>
+
+<img src="img/image.jpg" >
+</center>
+
+    <!-- / content body -->
+ </div>
+</div>
+</body>
+</html>
+
+<!-- Localized -->
