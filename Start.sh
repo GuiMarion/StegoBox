c8c8638c61d3162237fd0af4da16d990d589fa75
test <test@test.com>
2018-06-29 09:51:15 +0200

    working starting script

--- Start.sh
+++ Start.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-#shclient -4;
-
-#str= ifconfig | grep -m1 "inet"
-
-str="inet adr:10.250.100.241 Bcast"
-
 echo "Hi, you can find the webserver on : \n"
 
 echo "http://"
-echo -n $str | cut -d':' -f 2 | cut -d' ' -f 1 
+ifconfig | grep "inet" -m1 | cut -d ':' -f 2 | cut -d ' ' -f 1
 echo ":8080"
